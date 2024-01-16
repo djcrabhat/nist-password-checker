bd0b8d6a99acbc9178ef5f77d72a3f1f247f97e7
DJCrabhat <djcrabhat@sosimplerecords.com>
2024-01-15 17:16:43 -0800

    :technologist: add semantic-release and goreleaser (#9)

---
+++ .releaserc.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const fs = require("fs");
+
+const tplFile = path.resolve(__dirname, ".github/release-notes.hbs");
+
+module.exports = {
+  branches: ["main"],
+  tagFormat: "${version}",
+  plugins: [
+    [
+      "semantic-release-gitmoji",
+      {
+        releaseRules: {
+          patch: {
+            include: [
+              ":bento:",
+              ":arrow_up:",
+              ":lock:",
+              ":bug:",
+              ":ambulance:",
+            ],
+          },
+        },
+        releaseNotes: {
+          template: fs.readFileSync(tplFile, "utf-8"),
+        },
+      },
+    ],
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
+        message: [
+          ":bookmark: v${nextRelease.version} [skip ci]",
+          "",
+          "https://github.com/djcrabhat/nist-password-checker/releases/tag/${nextRelease.gitTag}",
+        ].join("\n"),
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        publishCmd:
+          'echo "${nextRelease.notes}" > ./release-notes.md' +
+          "\n" +
+          "goreleaser release --release-notes ./release-notes.md --clean",
+      },
+    ],
+  ],
+};

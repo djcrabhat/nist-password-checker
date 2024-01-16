d5375b40439b0658e43a5f122df54bee86bcff12
DJCrabhat <djcrabhat@sosimplerecords.com>
2024-01-15 16:46:14 -0800

    :technologist: add semantic-release

---
+++ .releaserc.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const fs = require('fs')
+
+const tplFile = path.resolve(__dirname, '.github/release-notes.hbs')
+
+module.exports = {
+    branches: [
+        "main",
+    ],
+    plugins: [
+        [
+            'semantic-release-gitmoji',
+            {
+                releaseRules: {
+                    patch: {
+                        include: [':bento:', ':arrow_up:', ':lock:', ':bug:', ':ambulance:'],
+                    },
+                },
+                releaseNotes: {
+                    template: fs.readFileSync(tplFile, 'utf-8'),
+                }
+            }
+        ],
+        '@semantic-release/github',
+        "@semantic-release/npm",
+        [
+            '@semantic-release/git',
+            {
+                message: [
+                    ':bookmark: v${nextRelease.version} [skip ci]',
+                    '',
+                    'https://github.com/djcrabhat/nist-password-checker/releases/tag/${nextRelease.gitTag}'
+                ].join('\n')
+            }
+        ]
+    ]
+}

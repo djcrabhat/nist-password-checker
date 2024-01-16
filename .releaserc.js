83efe45ab12cd514d688f370b0f978d954be4148
DJCrabhat <djcrabhat@sosimplerecords.com>
2024-01-15 17:06:21 -0800

    :technologist: add semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -1,37 +1,50 @@
-const path = require('path')
-const fs = require('fs')
+const path = require("path");
+const fs = require("fs");
 
-const tplFile = path.resolve(__dirname, '.github/release-notes.hbs')
+const tplFile = path.resolve(__dirname, ".github/release-notes.hbs");
 
 module.exports = {
-    branches: [
-        "main",
+  branches: ["main"],
+  plugins: [
+    [
+      "semantic-release-gitmoji",
+      {
+        releaseRules: {
+          patch: {
+            include: [
+              ":bento:",
+              ":arrow_up:",
+              ":lock:",
+              ":bug:",
+              ":ambulance:",
+            ],
+          },
+        },
+        releaseNotes: {
+          template: fs.readFileSync(tplFile, "utf-8"),
+        },
+      },
     ],
-    plugins: [
-        [
-            'semantic-release-gitmoji',
-            {
-                releaseRules: {
-                    patch: {
-                        include: [':bento:', ':arrow_up:', ':lock:', ':bug:', ':ambulance:'],
-                    },
-                },
-                releaseNotes: {
-                    template: fs.readFileSync(tplFile, 'utf-8'),
-                }
-            }
-        ],
-        '@semantic-release/github',
-        "@semantic-release/npm",
-        [
-            '@semantic-release/git',
-            {
-                message: [
-                    ':bookmark: v${nextRelease.version} [skip ci]',
-                    '',
-                    'https://github.com/djcrabhat/nist-password-checker/releases/tag/${nextRelease.gitTag}'
-                ].join('\n')
-            }
-        ]
-    ]
-}
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        message: [
+          ":bookmark: v${nextRelease.version} [skip ci]",
+          "",
+          "https://github.com/djcrabhat/nist-password-checker/releases/tag/${nextRelease.gitTag}",
+        ].join("\n"),
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        publishCmd:
+          'echo "${nextRelease.notes}" > /tmp/release-notes.md' +
+          "\n" +
+          "goreleaser release --release-notes /tmp/release-notes.md --clean",
+      },
+    ],
+  ],
+};
